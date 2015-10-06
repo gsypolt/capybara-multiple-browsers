332a28428648c8d4c0b580403abe78106480580e
Greg Sypolt <gsypolt@gannett.com>
2015-10-06 15:08:53 -0400

    added capabilities

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,10 +5,16 @@ require 'timeout'
 
 Capybara.default_wait_time = 10
 
-# To execute against other browsers pass environment variable from command line BROWSER=chrome or BROWSER=safari
+# To execute against other browsers pass environment variable from command line BROWSER=chrome
+# Safari is a work in progress
 Capybara.register_driver :selenium do |app|
+  capabilities = {
+        :nativeEvents => false,
+        :acceptSslCerts => true
+  }
+
   $browser = (ENV['BROWSER'] || 'firefox').to_sym
-  Capybara::Selenium::Driver.new(app, :browser => $browser)
+  Capybara::Selenium::Driver.new(app, :browser => $browser, :desired_capabilities => capabilities)
 
 end
 
